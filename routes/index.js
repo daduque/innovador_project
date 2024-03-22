e86956f0e2c0a90a32c14be6f21fe9580a62adb8
daduque <daanduque@utp.edu.co>
2024-03-22 15:29:14 -0500

    starting second challenge

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 const booksRouter = require('./books');
-const usersRouter = require('./routes/users');
+const usersRouter = require('./users');
 
 //use the router object to define the routes for the application
 
@@ -12,6 +12,6 @@ router.use('/', booksRouter);
 //admin routes
 
 //user routes
-app.use('/users', usersRouter);
+router.use('/users', usersRouter);
 
 module.exports = router;
