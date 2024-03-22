f0ed59315e757ae2ff9240f65710baaf2d58e471
daduque <daanduque@utp.edu.co>
2024-03-22 15:22:19 -0500

    responding with the list of books on json format

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 const booksRouter = require('./books');
+const usersRouter = require('./routes/users');
 
 //use the router object to define the routes for the application
 
@@ -11,4 +12,6 @@ router.use('/', booksRouter);
 //admin routes
 
 //user routes
+app.use('/users', usersRouter);
+
 module.exports = router;
