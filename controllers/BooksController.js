073d920e928781d938476cff4b30246639ef505c
Daniel Duque <daanduque@utp.edu.co>
2024-03-23 13:24:41 -0500

    challenge 3 complete!

--- controllers/BooksController.js
+++ controllers/BooksController.js
@@ -5,6 +5,12 @@ const path = require('path');
 
 const BooksController = {
     index: (req, res) => {
+        const message = 'ingreso en la ruta ' + req.url;
+        const dateTime = new Date().toLocaleString();
+        // console.log(dateTime + ' - ' + message);
+        //write the log message to the general_logs.txt file
+        fs.createWriteStream(path.join('logs', 'general_logs.txt'), { flags: 'a' });
+        fs.appendFileSync(path.join('logs', 'general_logs.txt'), dateTime + ' - ' + message + '\n');
         //read the file books.json from data directory and get the data
         let data = fs.readFileSync(path.join('data', 'books.json'), 'utf8');
         //parse the data to convert it into an array of objects
