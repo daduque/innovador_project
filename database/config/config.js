bd264bf2421e8b1e03aa7b8eb11e12ae1676980c
daduque <daanduque@utp.edu.co>
2024-03-30 18:21:23 -0500

    challenge 9th complete: remote db server connected

--- database/config/config.js
+++ database/config/config.js
@@ -1,9 +1,9 @@
 module.exports = {
   "development": {
-    "username": "root",
-    "password": 'root',
-    "database": "bookstore_db",
-    "host": "127.0.0.1",
+    "username": "sql10695242",
+    "password": 'p1aUdS4Bn7',
+    "database": "sql10695242",
+    "host": "sql10.freesqldatabase.com",
     "dialect": "mysql"
   },
   "test": {
