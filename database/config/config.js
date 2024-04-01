19b63930bf069afeb573ee946b85db180d4925ad
daduque <daanduque@utp.edu.co>
2024-03-31 19:04:53 -0500

    challenge 11th complete, express validation working like middleware on login form, custom validation for books cover is also on middleware

--- database/config/config.js
+++ database/config/config.js
@@ -14,10 +14,10 @@ module.exports = {
     "dialect": "mysql"
   },
   "production": {
-    "username": "root",
-    "password": null,
-    "database": "database_production",
-    "host": "127.0.0.1",
+    "username": "sql10695242",
+    "password": 'p1aUdS4Bn7',
+    "database": "sql10695242",
+    "host": "sql10.freesqldatabase.com",
     "dialect": "mysql"
   }
 }
